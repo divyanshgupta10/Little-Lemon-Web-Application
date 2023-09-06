fd4862aa894ee5b35a205d5638762eecf49596a4
divyanshgupta10 <143156139+divyanshgupta10@users.noreply.github.com>
2023-09-07 00:08:01 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(max_length=6)
+    booking_date = models.DateTimeField()
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(max_length=5)
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.SmallIntegerField()
+
+    def get_item(self):
+        return f'{self.title} : {str(self.price)}'
---
+++ serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Menu, Booking, MenuItem
+from django.contrib.auth.models import User
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
+        fields = ['id', 'title', 'price', 'inventory']
